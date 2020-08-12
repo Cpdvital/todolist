77b7c0cbfc0a317c8077f0237c6f834050c196ce
Sidney Almeida <sidney@cpdvital.com.br>
2020-08-12 08:34:17 -0300

    incluindo contatos

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,20 @@
   <div id="app">
     <div class="ui inverted vertical masthead center aligned segment">
       <div class="ui text container">
-        <h1 class="ui inverted header"> Aplicação Todo List com Vue.js 3</h1>
-        <h2 class="ui inverted header">Glaucia Lemos
-          <div class="sub header">Cloud Advocate em JavaScript/Node.js @ Microsoft</div>
+        <h1 class="ui inverted header"> Aplicação Todo List com Vue.js 3 by Glaucia Lemos</h1>
+        <h2 class="ui inverted header">Sidney Almeida
+          <div class="sub header">Analista de Sistemas</div>
         </h2>
-        <h3 class="ui inverted header">Rio de Janeiro, Brasil - 2020</h3>
-        <a class="tiny ui youtube button" type="button" href="https://www.youtube.com/user/l32759">
+        <h3 class="ui inverted header">São Paulo, Brasil - 2020</h3>
+        <a class="tiny ui youtube button" type="button" href="https://www.youtube.com/cpdvital">
           <i class="youtube icon"></i>
           YouTube
         </a>
-        <a class="tiny ui linkedin button" type="button" href="https://www.linkedin.com/in/glaucialemos/">
+        <a class="tiny ui linkedin button" type="button" href="https://www.linkedin.com/in/sidney-almeida/">
           <i class="linkedin icon"></i>
           LinkedIn
         </a>
-        <a class="tiny ui twitter button" type="button" href="https://twitter.com/glaucia_lemos86">
+        <a class="tiny ui twitter button" type="button" href="https://twitter.com/cpdvital">
           <i class="twitter icon"></i>
           Twitter
         </a>
